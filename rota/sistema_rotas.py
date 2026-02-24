46846d1280123a0c5bb9bdd4b87861ea84697fb2
louisedias <louisedias9@gmail.com>
2026-02-24 20:22:30 -0300

    using while

--- rota/sistema_rotas.py
+++ rota/sistema_rotas.py
@@ -1,13 +1,24 @@
-distance = float(input('Enter the distance between the two companies in kilometers(km): '))
-cost = (abs(distance)*5)
-is_correct = input('Is the distance correct? (YES/NO) ')
+continue_calc = 'yes'
 
-if is_correct.casefold() == 'yes':
-    print(f'The delivery cost is R${cost:.2f}')
+while continue_calc == 'yes' or continue_calc =="no":
 
-elif is_correct.casefold() == 'no':
-    distance = float(input('Enter the correct distance between the two companies in km: '))
+    
+
+    distance = float(input('Enter the distance between the two companies in kilometers(km): '))
     cost = (abs(distance)*5)
-    print(f'The delivery cost is R${cost:.2f}')
+    is_correct = input('Is the distance correct? (YES/NO) ')
+    
+    if is_correct.casefold() == 'yes':
+        print(f'The delivery cost is R${cost:.2f}')
 
-else:    print('Invalid input. Please enter YES or NO.')
+    elif is_correct.casefold() == 'no':
+        distance = float(input('Enter the correct distance between the two companies in km: '))
+        cost = (abs(distance)*5)
+        print(f'The delivery cost is R${cost:.2f}')
+
+    else: print('Invalid input. Please enter YES or NO.')
+
+    continue_calc = input('Do you want to recalculate the delivery cost? (YES/NO) ')
+    if continue_calc.casefold() == 'no':
+        print('Thank you for using the delivery cost calculator. Goodbye!')
+        break
