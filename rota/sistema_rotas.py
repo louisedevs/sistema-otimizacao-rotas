7b411e9bb65960dd79737b1287e0b8f338760674
louisedias <louisedias9@gmail.com>
2026-02-24 20:44:15 -0300

    using while and array

--- rota/sistema_rotas.py
+++ rota/sistema_rotas.py
@@ -1,20 +1,23 @@
+history = []
+
 continue_calc = 'yes'
 
 while continue_calc == 'yes' or continue_calc =="no":
-
-    
 
     distance = float(input('Enter the distance between the two companies in kilometers(km): '))
     cost = (abs(distance)*5)
     is_correct = input('Is the distance correct? (YES/NO) ')
     
+    
     if is_correct.casefold() == 'yes':
         print(f'The delivery cost is R${cost:.2f}')
+        history.append({'distance': distance, 'cost': cost})
 
     elif is_correct.casefold() == 'no':
         distance = float(input('Enter the correct distance between the two companies in km: '))
         cost = (abs(distance)*5)
         print(f'The delivery cost is R${cost:.2f}')
+        history.append({'distance': distance, 'cost': cost})
 
     else: print('Invalid input. Please enter YES or NO.')
 
@@ -22,3 +25,4 @@ while continue_calc == 'yes' or continue_calc =="no":
     if continue_calc.casefold() == 'no':
         print('Thank you for using the delivery cost calculator. Goodbye!')
         break
+
